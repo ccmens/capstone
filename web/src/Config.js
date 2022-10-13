78e56e74f2802b77212aea0dbe2bc9498ad8534f
ccmens <108434978+ccmens@users.noreply.github.com>
2022-10-13 15:35:19 -0600

    Update Config.js

--- web/src/Config.js
+++ web/src/Config.js
@@ -2,7 +2,7 @@ export const Config = {
     appName: 'Final Project',
     // Thr front base url
     appBaseUrl: 'http://localhost:4000/',
-    // appBaseUrl: 'http://group5.app-super.link/',
+
     apiUser: 'finalproject',
     // connect api key, most important
     apiKey: 'ce90b9d7-2422-4e79-9e47-49fe80f9e623',
