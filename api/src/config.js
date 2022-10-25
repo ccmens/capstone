b4c977063d1e7d9700e5cc7f1cb2f64368dc2db2
Calzar1001 <85040148+Calzar1001@users.noreply.github.com>
2022-10-25 08:28:12 -0600

    Updated personalDB connection
    added gitignore for testing the database connection issue

--- api/src/config.js
+++ api/src/config.js
@@ -1,17 +1,17 @@
 const config = {
     server_host: 'localhost',
     server_port: 4000,
-    database_url: 'mongodb+srv://VO_User1:1vcafgpyZp08diEh@voinventorycluster.dglj9c2.mongodb.net/?retryWrites=true&w=majority',
+    database_url: 'mongodb+srv://VO_User2:RwH.bXD7bv6-aQA@voinventorycluster.dglj9c2.mongodb.net/?retryWrites=true&w=majority',
     prefix: '/api',
     verion: '1.0',
     http_url: 'http://localhost:4000',
-    
+
     is_server: false,
     access_token_secret: '269262d1ce5a13ff880ab731ddb044d47035031d3468b9fb79092db3aea6eaddb4cdb5469f8a34c973353bd8c76358d2bd86926a49edbcfce620d53a5c0a2691',
     access_token_expires_in: '24h',
     without_check_token: [
-        '/api/user/login', 
-        '/api/user/tokenLogin', 
+        '/api/user/login',
+        '/api/user/tokenLogin',
         '/api/user/register',
         '/favicon.ico',
     ],
