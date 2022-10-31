9cbbaa3db6de367bfc90baf0ca54a2122d37ee67
SeanLee <92111656+SeanLee0033@users.noreply.github.com>
2022-10-31 12:49:16 -0600

    added vo link

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -12,8 +12,9 @@ export default function Footer() {
             <div className="footer-three">
             </div>
             <div className="footer-four">
+                <a href="https://www.instagram.com/var.osc/"><img src="/images/logo.jpg" width={40} /></a>
                 <a href="https://www.etsy.com/shop/VariableOscillations"><img src="/images/etsy.jpg" width={70} /></a>
-                <a href="https://www.instagram.com/var.osc/"><img src="/images/insta.png" width={40} /></a>
+                <a href="https://www.instagram.com/var.osc/"><img src="/images/insta.png" width={35} /></a>
             </div>
         </footer>
     )
