38a782369161fc9829f910da4f1876c845b84cbc
Calzar1001 <85040148+Calzar1001@users.noreply.github.com>
2022-11-02 11:44:44 -0600

    Updated Item model
    Fields added:
    digikey_part_num
    stock
    supplier_link

--- api/src/models/item.model.js
+++ api/src/models/item.model.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
+// Database model for MongoDB
 const itemSchema = new mongoose.Schema({
     item_name: { type: String },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'category' },
     price: { type: Number },
+    stock: { type: Number },
+    digikey_part_num: { type: Number },
+    supplier_link: { type: String },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     deleted: { type: Boolean, default: false },
-    image: {type: String},
+    image: { type: String },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 })
