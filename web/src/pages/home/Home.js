baf2d8aa49c49e85eb35d762bfaef16c42acb407
Apon Pham Tran <pham.tran@edu.sait.ca>
2022-11-07 20:20:14 -0700

    Update
    Boxes added

--- web/src/pages/home/Home.js
+++ web/src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css'
+<<<<<<< Updated upstream
 
 import Grid from "@mui/material/Grid";
 import MDBox from "../../components/MDBox";
@@ -134,3 +135,34 @@ const Home = ({user}) => {
 }
 
 export default Home;
+=======
+import BannerSection from "../../components/BannerSection";
+import { Dashboad } from '../../components/DashBoard';
+import contents from './DashBoard';
+import { Stacked } from "../../components/Charts/Stacked";
+// import { Bar, state } from '../../components/Charts/BarChart';
+// import { Linechart } from "../../components/Charts/LineChart";
+
+export default function Home({user}) {
+    return (
+        <>
+            {/*{user && user.role?.role_name === 'admin' && <AdminManageBar user={user}/>}*/}
+            {/*{user && user.role?.role_name === 'user' && <UserManageBar user={user}/>}*/}
+            <BannerSection title="Welcome To Variable Oscillations-Inventory MGMT" color='white' />
+            
+            <div className='App'>
+                {contents.map(contents => (
+                    <Dashboad 
+                    key={contents.id}
+                    name={contents.name}
+                    unitLeft={contents.unitLeft}
+                    />
+                ))}
+            </div>
+            <div>
+                <Stacked width="320px" height="360px" />
+            </div>
+        </>
+    );
+}
+>>>>>>> Stashed changes
---
+++ web/src/pages/lineChart/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+import LineChart from './LineChart.js';
+
+ReactDOM.render(
+    <LineChart />, 
+    document.getElementById('root')
+);
