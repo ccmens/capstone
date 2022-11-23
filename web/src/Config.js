dcf53e7651f1078d8121fdd35ef6a9592849a9d7
ccmens <108434978+ccmens@users.noreply.github.com>
2022-11-22 22:40:46 -0700

    Merge branch 'NeededParts-Testing' into Nested-NeededParts

--- web/src/Config.js
+++ web/src/Config.js
@@ -2,7 +2,6 @@ export const Config = {
     appName: 'Final Project',
     // Thr front base url
     appBaseUrl: 'http://localhost:4000/',
-
     apiUser: 'finalproject',
     // connect api key, most important
     apiKey: 'ce90b9d7-2422-4e79-9e47-49fe80f9e623',
