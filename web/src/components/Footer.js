8a8192ac57c47f93e2a4fca1b6aa9390a17b8235
SeanLee0033 <seanlee.nov@gmail.com>
2022-11-30 22:36:56 -0700

    edited footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="footer-one">
                 &copy; Copyright 2022 & Capstone Project
             </div>
-            <div className="footer-four">
+            <div className="footer-two">
                 <a href="http://varosc.net/"><img src="/images/logo.jpg" width={40} /></a>
                 <a href="https://www.etsy.com/shop/VariableOscillations"><img src="/images/etsy.jpg" width={70} /></a>
                 <a href="https://www.instagram.com/var.osc/"><img src="/images/insta.png" width={35} /></a>
